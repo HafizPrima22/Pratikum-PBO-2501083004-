c62480f7d5a8a50a93e7ba7c795f8ee8fff27782
HafizPrima22 <hafizprimaa22@gmail.com>
2026-07-03 23:35:19 +0700

    Pratikum150626

---
+++ Pratikum150626/Pratikum150626.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pratikum150626;
+
+/**
+ *
+ * @author LAB-SI-PC
+ */
+public class Pratikum150626 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
